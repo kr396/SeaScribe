844540d8a8b3d7ab8940d04af42097cc3c3f68ff
Kaushalkumar <60640854+kr396@users.noreply.github.com>
2024-02-02 15:14:59 +0530

    Splash screen and android app icon

--- App.tsx
+++ App.tsx
@@ -5,17 +5,22 @@
  * @format
  */
 
-import React from 'react';
+import React, {useEffect} from 'react';
 import {Provider} from 'react-redux';
 
 import {PersistGate} from 'redux-persist/integration/react';
 import FlipperAsyncStorage from 'rn-flipper-async-storage-advanced';
+import SplashScreen from 'react-native-splash-screen';
+import {RealmProvider} from '@realm/react';
 
 import Router from './src/navigation';
 import {persistor, store} from './src/store';
-import {RealmProvider} from '@realm/react';
 
 function App(): React.JSX.Element {
+  useEffect(() => {
+    SplashScreen.hide();
+  }, []);
+
   return (
     <RealmProvider>
       <Provider store={store}>
